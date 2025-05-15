f5c3122e36917c8576a9e3ab55823cf198390118
Ilkhom Raim <ilkhomrm@gmail.com>
2025-05-15 22:11:27 +0500

    VUE-shoes Header

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="flex justify-between border-b border-slate-300 px-16 py-8">
+    <div class="flex items-center gap-4">
+      <img class="w-10" src="/logo.png" alt="logo" />
+      <div>
+        <h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
+        <p class="text-slate-400">Магазин худших кросовок</p>
+      </div>
+    </div>
+
+    <ul class="flex items-center gap-10">
+      <li class="flex items-center gap-3 text-gray-500 hover:text-black cursor-pointer">
+        <img src="/cart.svg" alt="" />
+        <b>1205 руб.</b>
+      </li>
+      <li class="flex items-center gap-3 text-gray-500 hover:text-black cursor-pointer">
+        <img src="/heart.svg" alt="" />
+        <span>Закладки</span>
+      </li>
+      <li class="flex items-center gap-3 text-gray-500 hover:text-black cursor-pointer">
+        <img src="/profile.svg" alt="" />
+        <span>Профиль</span>
+      </li>
+    </ul>
+  </header>
+</template>
