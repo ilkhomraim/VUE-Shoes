26dd94c5102ac67694652c13dd6e44acec91bc09
Ilkhom Raim <ilkhomrm@gmail.com>
2025-05-16 11:57:18 +0500

    VUE-shoes Card

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup>
 import Header from './components/Header.vue'
+import CardList from './components/CardList.vue'
 </script>
 
 <template>
-  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-10">
+  <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-10">
     <Header />
+
+    <div class="p-10">
+      <h2 class="text-3xl font-bold mb-8">Все Кроссовки</h2>
+
+      <CardList />
+    </div>
   </div>
 </template>
 
---
+++ src/components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="relative bg-white border-slate-300 border-solid rounded-3xl p-8 cursor-pointer hover:-translate-y-2 hover:shadow-xl transition"
+  >
+    <div>
+      <img class="abosolute top-8 left-8" src="/like-1.svg" alt="" />
+      <img src="/sneakers/sneakers-1.jpg" alt="Sneaker" />
+      <p class="mt-2">Мужские Кроссовки Nike Blazer Mid Suede</p>
+    </div>
+
+    <div class="flex justify-between mt-5">
+      <div class="flex flex-col">
+        <span class="text-slate-500">Цена:</span>
+        <span class="font-bold">12 999 руб.</span>
+      </div>
+      <img src="/plus.svg" alt="" />
+    </div>
+  </div>
+</template>
