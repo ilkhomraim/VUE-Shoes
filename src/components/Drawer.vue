d0008da7fd1ec2544745c3afb384703ff5970bda
Ilkhom Raim <ilkhomrm@gmail.com>
2025-05-17 02:35:19 +0500

    VUE-shoes Drawer

---
+++ src/components/Drawer.vue
@@ -0,0 +1,15 @@
+<script setup>
+import DrawerHead from './DrawerHead.vue'
+import CartItem from './CartItem.vue'
+</script>
+
+<template>
+  <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-70 mb-4"></div>
+  <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-7 gap-10">
+    <DrawerHead />
+
+    <CartItem />
+    <CartItem />
+    <CartItem />
+  </div>
+</template>
