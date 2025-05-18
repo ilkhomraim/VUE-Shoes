e2527e53dccadd949b976639b6fd495967589076
Ilkhom Raim <ilkhomrm@gmail.com>
2025-05-18 13:22:16 +0500

    VUE-shoes Search

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,30 @@ import Drawer from './components/Drawer.vue'
 </script>
 
 <template>
-  <Drawer />
+  <!-- <Drawer /> -->
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-10">
     <Header />
 
     <div class="p-10">
-      <h2 class="text-3xl font-bold mb-8">Все Кроссовки</h2>
+      <div class="flex justify-between items-center mb-8">
+        <h2 class="text-3xl font-bold">Все Кроссовки</h2>
+
+        <select
+          class="py-2 px-3 border border-gray-200 rounded-md outline focus:border-gray-300 transition outline-none"
+        >
+          <option>По названию</option>
+          <option>По цене (дешевые)</option>
+          <option>По цене (дорогие)</option>
+        </select>
+
+        <div class="relative">
+          <img class="absolute left-3 top-3" src="/search.svg" alt="" />
+          <input
+            class="border border-gray-200 rounded-md py-2 pl-12 pr-4 outline-none focus:border-gray-300 transition"
+            placeholder="Поиск"
+          />
+        </div>
+      </div>
 
       <CardList />
     </div>
