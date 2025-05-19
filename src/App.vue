f136e9447bc88bd8c679cdfcdd9f149dfed3f370
Ilkhom Raim <ilkhomrm@gmail.com>
2025-05-19 14:04:49 +0500

    VUE-shoes SortBy

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import axios from 'axios'
 
 import Header from './components/Header.vue'
@@ -8,9 +8,28 @@ import Drawer from './components/Drawer.vue'
 
 const items = ref([])
 
+const sortBy = ref()
+const searchQuery = ref()
+
+const onChangeSelect = (event) => {
+  sortBy.value = event.target.value
+}
+
 onMounted(async () => {
   try {
     const { data } = await axios.get('https://fa3088e6425c2332.mokky.dev/items')
+
+    items.value = data
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+watch(sortBy, async () => {
+  try {
+    const { data } = await axios.get(
+      'https://fa3088e6425c2332.mokky.dev/items?sortBy=' + sortBy.value,
+    )
 
     items.value = data
   } catch (err) {
@@ -29,11 +48,12 @@ onMounted(async () => {
         <h2 class="text-3xl font-bold">Все Кроссовки</h2>
 
         <select
+          @change="onChangeSelect"
           class="py-2 px-3 border border-gray-200 rounded-md outline focus:border-gray-300 transition outline-none"
         >
-          <option>По названию</option>
-          <option>По цене (дешевые)</option>
-          <option>По цене (дорогие)</option>
+          <option value="name">По названию</option>
+          <option value="price">По цене (дешевые)</option>
+          <option value="-price">По цене (дорогие)</option>
         </select>
 
         <div class="relative">
